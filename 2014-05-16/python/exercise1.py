32dd6533d7033a7c0be17ed0a7c79d544bac042a
andreadago <and.dago@hotmail.it>
2014-05-16 23:47:13 +0200

    end exercise 1

--- 2014-05-16/python/exercise1.py
+++ 2014-05-16/python/exercise1.py
@@ -53,23 +53,100 @@ master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemo
 #DRAW(master)
 
 """Kitchen - aka the room down on the right"""
+#kitchen door
 toMerge = 29
-diagram = assemblyDiagramInit([1,3,2])([[.3],[3,1,3],[2.4,.3]])
-master = diagram2cell(diagram,master,toMerge)
+doorDiagram_kit = assemblyDiagramInit([1,3,2])([[.3],[3,1,3],[2.4,.3]])
+master = diagram2cell(doorDiagram_kit,master,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master)))
 hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-VIEW(hpc)
+#VIEW(hpc)
 
 toRemove = [113]
 master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
 
+#kitchen doorwindow
+toMerge = 57
+doorwindowDiagram_kit = assemblyDiagramInit([1,3,3])([[.3],[3,1,3],[.5,2,.3]])
+master = diagram2cell(doorwindowDiagram_kit,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+toRemove = [119]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
 
 
+"""Room - the room top on the left"""
+#Room door
+toMerge = 20
+doorDiagram = assemblyDiagramInit([3,1,2])([[3,1.5,3],[.3],[2.4,.3]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+
+toRemove = [124]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+
+#Room window
+toMerge = 10
+windowDiagram = assemblyDiagramInit([1,8,5])([[.3],[.1,1,.1,1,.1,1,.1,1],[.3,1,.1,1,.5]])
+master = diagram2cell(windowDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2) 
+
+
+toRemove = [154,144]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+"""Bathroom"""
+#Bathroom door
+toMerge = 34
+doorDiagram_kit = assemblyDiagramInit([1,3,2])([[.3],[3,2,3],[2.4,.3]])
+master = diagram2cell(doorDiagram_kit,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+
+toRemove = [165]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+#Bathroom window
+toMerge = 61
+windowDiagram = assemblyDiagramInit([1,8,5])([[.3],[.1,1,.1,1,.1,1,.1,1],[.3,1,.1,1,.5]])
+master = diagram2cell(windowDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2) 
+
+toRemove = [195]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+"""Small room"""
+#Small room door
+toMerge = 30
+doorDiagram = assemblyDiagramInit([1,3,2])([[.3],[3,2,3],[2.4,.3]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+toRemove = [207]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+#Small room window
+toMerge = 56
+windowDiagram = assemblyDiagramInit([1,8,3])([[.3],[.1,1,.1,1,.1,1,.1,1],[.3,1,.5]])
+master = diagram2cell(windowDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2) 
+
+toRemove = [225, 219]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+
+
+VIEW(hpc)
 DRAW(master)
-
-
-#toRemove = [26]
-#hall = hall[0], [cell for k,cell in enumerate(hall[1]) if not (k in toRemove)]
-
-#VIEW(hpc)
-#DRAW(master)
