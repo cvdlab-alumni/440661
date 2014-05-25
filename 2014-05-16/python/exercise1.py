347f589a03cf0684e4eb5c25f41965d3d527ba12
andreadago <and.dago@hotmail.it>
2014-05-25 16:29:20 +0200

    more details

--- 2014-05-16/python/exercise1.py
+++ 2014-05-16/python/exercise1.py
@@ -15,138 +15,339 @@ from myfont import *
 from architectural import *
 from sysml import *
 
+def MK_TRNSP(hpc):
+    return MATERIAL([1,1,1,0.1, 0,0,0.8,0.5, 1,1,1,0.1, 1,1,1,0.1, 100])(hpc)
+
 DRAW = COMP([VIEW,STRUCT,MKPOLS])
 
-master = assemblyDiagramInit([5,8,2])([[.3,6,.1,5,.3],[.3,8,.3,4,.3,5,.3,.3],[.3,2.7]])
+#Colors
+WINDOW = [0.686, 0.933, 0.933]
+DOOR = [0.545, 0.271, 0.075]
+
+master = assemblyDiagramInit([13,11,3])([[.3,2,.3,4,.3,4,.1,3,.1,2,.3,2,.3],[.3,5,.1,2,.1,3,.3,2,.1,3,.3],[.3,1.2,1.5]])
+#master = assemblyDiagramInit([5,8,2])([[.3,6,.1,5,.3],[.3,8,.3,4,.3,5,.3,.3],[.3,2.7]])
+
 V,CV = master
 hpc = SKEL_1(STRUCT(MKPOLS(master)))
 hpc = cellNumbering (master,hpc)(range(len(CV)),CYAN,2)
 #VIEW(hpc)
 
-toRemove = [19,51,23,55,27,59,21]
+toRemove = [38,37,35,2,5,8,11,14,41,40,44,43,47,50,49,17,53,20,46, #balconeSX
+			56,55,62,61,104,103,106,107,110,109,112,113,115,116,122,121,128,127,
+			124,125,136,137,142,143,148,149,169,170,172,173,175,176,178,179,181,182,
+			140,139,145,146,187,188,194,193,221,220,254,253,320,319,322,323,325,326,
+			292,293,259,260,248,247,245,244,242,241,307,308,235,236,268,269,301,302,
+			390,391,392,393,394,395,426,427,428,423,424,425,387,388,389,420,421,422,384,385,386,
+			417,418,419,383,416,379,380,413,314,313,310,311,#parte superiore+parte nel mezzo
+			377,410,407,374,373,371,370,404,368,367,401,398,365,#balconeDX
+			21,22,23,54,55,24,25,26,27,28,29,30,31,32,63,64,65,60,61,62,57,58,59,376,377#parte in alto a sx
+			]
 master = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
 #DRAW(master)
-
+#VIEW(hpc)
 
 """Hall - the room down on the left"""
 #Hall Door
-toMerge = 17
-doorDiagram = assemblyDiagramInit([3,1,2])([[3,1.5,3],[.3],[2.4,.3]])
+toMerge = 98
+doorDiagram = assemblyDiagramInit([3,1,2])([[3,2,2.5],[.3],[2,.7]])
 master = diagram2cell(doorDiagram,master,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master)))
 hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
 
-toRemove = [74]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+toMerge = 97
+doorDiagram = assemblyDiagramInit([3,1,2])([[3,2,2.5],[.3],[2,.5]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
 #VIEW(hpc)
 
+
+#toRemove = [271,278,277]
+#master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+#########################STRUCT hall_door
+hall_door = [271,278,277]
+hall_door = master[0], [cell for k,cell in enumerate(master[1]) if (k in hall_door)]
+hall_door = COLOR(DOOR)((STRUCT(MKPOLS(hall_door))))
+#########################
+toRemove = [271,278,277]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+#VIEW(hall_door)
+
+#VIEW(hpc)
+#DRAW(master)
+
 #Hall window
-toMerge = 3
-windowDiagram = assemblyDiagramInit([1,8,5])([[.3],[.1,1,.1,1,.1,1,.1,1],[.3,1,.1,1,.5]])
+toMerge = 28
+windowDiagram = assemblyDiagramInit([1,8,5])([[.3],[.1,.5,.1,.5,.1,.5,.1,.5],[.3,1,.1,1,.5]])
 master = diagram2cell(windowDiagram,master,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master)))
 hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2) 
 #VIEW(hpc)
 
-toRemove = [94,104,92,102]
+#####################################STRUCT hall_window
+hall_window = [315,313,305,303,295,293]
+hall_window = master[0], [cell for k,cell in enumerate(master[1]) if (k in hall_window)]
+hall_window = COLOR(WINDOW)((STRUCT(MKPOLS(hall_window))))
+hall_window = MK_TRNSP(hall_window)
+#####################################
+toRemove = [315,313,305,303,295,293]
 master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-#DRAW(master)
+
+#VIEW(hpc)
+
+#Hall doorwindow
+toMerge = 38
+doorDiagram = assemblyDiagramInit([1,3,2])([[.3],[1,1.5,2],[2.4,.5]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+toMerge = 38
+doorDiagram = assemblyDiagramInit([1,3,2])([[.3],[1,1.5,2],[2.4,.5]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+##########################
+hall_doorw = [317,312,311]
+hall_doorw = master[0], [cell for k,cell in enumerate(master[1]) if (k in hall_doorw)]
+hall_doorw = COLOR(WINDOW)((STRUCT(MKPOLS(hall_doorw))))
+hall_doorw = MK_TRNSP(hall_doorw)
+##########################
+
+toRemove = [317,312,311]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
 
 """Kitchen - aka the room down on the right"""
 #kitchen door
-toMerge = 29
-doorDiagram_kit = assemblyDiagramInit([1,3,2])([[.3],[3,1,3],[2.4,.3]])
+toMerge = 115
+doorDiagram_kit = assemblyDiagramInit([1,3,2])([[.3],[3,1.5,3],[2.1,.3]])
+master = diagram2cell(doorDiagram_kit,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+toMerge = 114
+doorDiagram_kit = assemblyDiagramInit([1,3,2])([[.3],[3,1.5,3],[2.1,.3]])
 master = diagram2cell(doorDiagram_kit,master,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master)))
 hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
 #VIEW(hpc)
 
-toRemove = [113]
+################################
+kitch_door = [318,324,325]
+kitch_door = master[0], [cell for k,cell in enumerate(master[1]) if (k in kitch_door)]
+kitch_door = COLOR(DOOR)((STRUCT(MKPOLS(kitch_door))))
+################################
+toRemove = [318,324,325]
 master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
 
 #kitchen doorwindow
-toMerge = 57
-doorwindowDiagram_kit = assemblyDiagramInit([1,3,3])([[.3],[3,1,3],[.5,2,.3]])
+toMerge = 212
+doorwindowDiagram_kit = assemblyDiagramInit([1,3,3])([[.3],[3,1.5,3],[.5,2,.3]])
 master = diagram2cell(doorwindowDiagram_kit,master,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master)))
 hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-toRemove = [119]
+
+toMerge = 212
+doorwindowDiagram_kit = assemblyDiagramInit([1,3,3])([[.3],[3,1.5,3],[.5,2,.3]])
+master = diagram2cell(doorwindowDiagram_kit,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+################################
+kitch_doorw = [335,336,327]
+kitch_doorw = master[0], [cell for k,cell in enumerate(master[1]) if (k in kitch_doorw)]
+kitch_doorw = COLOR(WINDOW)((STRUCT(MKPOLS(kitch_doorw))))
+kitch_doorw = MK_TRNSP(kitch_doorw)
+################################
+
+toRemove = [335,336,327]
 master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+"""Corridor"""
+#south door - the door on the hall
+toMerge = 100
+doorDiagram = assemblyDiagramInit([3,1,2])([[3,2,1.5],[.3],[2,.7]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+toMerge = 100
+doorDiagram = assemblyDiagramInit([3,1,2])([[3,2,1.5],[.3],[2,.5]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+#########################################
+s_door = [338,339,344]
+s_door = master[0], [cell for k,cell in enumerate(master[1]) if (k in s_door)]
+s_door = COLOR(DOOR)((STRUCT(MKPOLS(s_door))))
+#########################################
+toRemove = [338,339,344]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+#north door - the door on the top bathroom
+toMerge = 102
+doorDiagram = assemblyDiagramInit([3,1,2])([[3,2,1.5],[.3],[2,.7]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+toMerge = 102
+doorDiagram = assemblyDiagramInit([3,1,2])([[3,2,1.5],[.3],[2,.7]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+#########################################
+n_door = [351,345,346]
+n_door = master[0], [cell for k,cell in enumerate(master[1]) if (k in n_door)]
+n_door = COLOR(DOOR)((STRUCT(MKPOLS(n_door))))
+#########################################
+
+toRemove = [351,345,346]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+#west door - the door on the left room
+toMerge = 82
+doorDiagram = assemblyDiagramInit([1,3,2])([[.3],[.3,1,.3],[2.4,.5]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+toMerge = 82
+doorDiagram = assemblyDiagramInit([1,3,2])([[.3],[.3,1,.3],[2.4,.5]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+#########################################
+w_door = [352,353,358]
+w_door = master[0], [cell for k,cell in enumerate(master[1]) if (k in w_door)]
+w_door = COLOR(DOOR)((STRUCT(MKPOLS(w_door))))
+#########################################
+
+toRemove = [352,353,358]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+#south west door - the door on the bottom room
+toMerge = 144
+doorDiagram = assemblyDiagramInit([3,1,2])([[3,2,1.5],[.3],[2,.7]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+toMerge = 144
+doorDiagram = assemblyDiagramInit([3,1,2])([[3,2,1.5],[.3],[2,.5]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+#########################################
+sw_door = [359,360,365]
+sw_door = master[0], [cell for k,cell in enumerate(master[1]) if (k in sw_door)]
+sw_door = COLOR(DOOR)((STRUCT(MKPOLS(sw_door))))
+#########################################
+
+
+toRemove = [359,360,365]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+#east door - the door on the top room
+toMerge = 173
+doorDiagram = assemblyDiagramInit([1,3,2])([[.3],[.3,1,.3],[2.4,.5]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+toMerge = 172
+doorDiagram = assemblyDiagramInit([1,3,2])([[.3],[.3,1,.3],[2.4,.5]])
+master = diagram2cell(doorDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+
+#########################################
+e_door = [372,373,366]
+e_door = master[0], [cell for k,cell in enumerate(master[1]) if (k in e_door)]
+e_door = COLOR(DOOR)((STRUCT(MKPOLS(e_door))))
+#########################################
+
+toRemove = [372,373,366]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
 
 
 """Room - the room top on the left"""
-#Room door
-toMerge = 20
-doorDiagram = assemblyDiagramInit([3,1,2])([[3,1.5,3],[.3],[2.4,.3]])
-master = diagram2cell(doorDiagram,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-
-
-toRemove = [124]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-
-
-
 #Room window
-toMerge = 10
-windowDiagram = assemblyDiagramInit([1,8,5])([[.3],[.1,1,.1,1,.1,1,.1,1],[.3,1,.1,1,.5]])
+toMerge = 49
+windowDiagram = assemblyDiagramInit([1,5,5])([[.3],[1.1,1,.1,1,1.1],[.3,1,.1,1,.5]])
 master = diagram2cell(windowDiagram,master,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master)))
 hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2) 
 
+#########################################
+room_win = [390,380,379,378,389,388]
+room_win = master[0], [cell for k,cell in enumerate(master[1]) if (k in room_win)]
+room_win = COLOR(WINDOW)((STRUCT(MKPOLS(room_win))))
+room_win = MK_TRNSP(room_win)
+#########################################
 
-toRemove = [154,144]
+toRemove = [390,380,379,378,389,388]
 master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
 
 
-"""Bathroom"""
-#Bathroom door
-toMerge = 34
-doorDiagram_kit = assemblyDiagramInit([1,3,2])([[.3],[3,2,3],[2.4,.3]])
-master = diagram2cell(doorDiagram_kit,master,toMerge)
+"""Room - the room top on the right"""
+#Room window
+toMerge = 224
+windowDiagram = assemblyDiagramInit([1,5,5])([[.3],[1.1,1,.1,1,1.1],[.3,1,.1,1,.5]])
+master = diagram2cell(windowDiagram,master,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master)))
+hpc = cellNumbering (master,hpc)(range(len(master[1])),RED,3) 
+
+#########################################
+room_winr = [398,408,396,397,407,406]
+room_winr = master[0], [cell for k,cell in enumerate(master[1]) if (k in room_winr)]
+room_winr = COLOR(WINDOW)((STRUCT(MKPOLS(room_winr))))
+room_winr = MK_TRNSP(room_winr)
+#########################################
+
+toRemove = [398,408,396,397,407,406, 166,165,163,162,160,159]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+
+
+"""Room-the bottom room"""
+toMerge = 200
+doorwindowDiagram_kit = assemblyDiagramInit([1,3,3])([[.3],[.5,1,.5],[.5,2,.3]])
+master = diagram2cell(doorwindowDiagram_kit,master,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master)))
 hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
 
-
-toRemove = [165]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-
-#Bathroom window
-toMerge = 61
-windowDiagram = assemblyDiagramInit([1,8,5])([[.3],[.1,1,.1,1,.1,1,.1,1],[.3,1,.1,1,.5]])
-master = diagram2cell(windowDiagram,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2) 
-
-toRemove = [195]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-
-
-"""Small room"""
-#Small room door
-toMerge = 30
-doorDiagram = assemblyDiagramInit([1,3,2])([[.3],[3,2,3],[2.4,.3]])
-master = diagram2cell(doorDiagram,master,toMerge)
+toMerge = 199
+doorwindowDiagram_kit = assemblyDiagramInit([1,3,3])([[.3],[.5,1,.5],[.5,2,.3]])
+master = diagram2cell(doorwindowDiagram_kit,master,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master)))
 hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
 
-toRemove = [207]
+#########################################
+room_win2 = [405,414,404]
+room_win2 = master[0], [cell for k,cell in enumerate(master[1]) if (k in room_win2)]
+room_win2 = COLOR(WINDOW)((STRUCT(MKPOLS(room_win2))))
+room_win2 = MK_TRNSP(room_win2)
+#########################################
+
+toRemove = [405,414,404]
 master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
 
+#VIEW(hpc)
 
-#Small room window
-toMerge = 56
-windowDiagram = assemblyDiagramInit([1,8,3])([[.3],[.1,1,.1,1,.1,1,.1,1],[.3,1,.5]])
-master = diagram2cell(windowDiagram,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2) 
-
-toRemove = [225, 219]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+#################################
 
 
+#VIEW(master)
 
-
-VIEW(hpc)
-DRAW(master)
+#DRAW(master)
--- 2014-05-16/python/exercise2.py
+++ 2014-05-16/python/exercise2.py
@@ -3,67 +3,98 @@ from splines import *
 pink9 = [1,0.855,0.725]
 
 """Top of the dwelling"""
-top = assemblyDiagramInit([5,8,2])([[.3,6,.1,5,.3],[.3,8,.3,4,.3,5,.3,.3],[.3,2.7]])
+top = assemblyDiagramInit([3,3,2])([[.3,14.1,.3],[.3,15.6,.3],[.3,2.7]])
 V,CV = top
 hpc = SKEL_1(STRUCT(MKPOLS(top)))
 hpc = cellNumbering (top,hpc)(range(len(CV)),CYAN,2)
 
 """Corridor"""
-corridor = assemblyDiagramInit([3,3,2])([[.3,11,.3],[.3,17.9,.3],[.3,2.7]])
+###################################
+#Corridor_base
+Corridor_base = assemblyDiagramInit([5,3,2])([[.3,4.9,.1,8.8,.3],[.3,5,.3],[.3,3.7]]) #x=13.8, y=15.6
+V,CV = Corridor_base
+hpc_corridor_base = SKEL_1(STRUCT(MKPOLS(Corridor_base)))
+hpc_corridor_base = cellNumbering (Corridor_base,hpc_corridor_base)(range(len(CV)),CYAN,2)
+#VIEW(hpc_corridor)
+
+toRemove = [9,15,21]
+Corridor_base = Corridor_base[0], [cell for k,cell in enumerate(Corridor_base[1]) if not (k in toRemove)]
+
+#Door1
+toMerge = 20
+doorDiagram = assemblyDiagramInit([3,1,2])([[1.3,1,6.5],[.3],[2.4,.3]])
+Corridor_base = diagram2cell(doorDiagram,Corridor_base,toMerge)
+hpc_corridor_base = SKEL_1(STRUCT(MKPOLS(Corridor_base)))
+hpc_corridor_base = cellNumbering (Corridor_base,hpc_corridor_base)(range(len(Corridor_base[1])),CYAN,2)
+
+toRemove = [28]
+Corridor_base = Corridor_base[0], [cell for k,cell in enumerate(Corridor_base[1]) if not (k in toRemove)]
+
+###################################
+#Corridor
+corridor = assemblyDiagramInit([5,3,2])([[.3,4.9,.1,8.8,.3],[.3,5,.3],[.3,2.7]]) #x=13.8, y=15.6
 V,CV = corridor
 hpc_corridor = SKEL_1(STRUCT(MKPOLS(corridor)))
 hpc_corridor = cellNumbering (corridor,hpc_corridor)(range(len(CV)),CYAN,2)
 #VIEW(hpc_corridor)
 
-toRemove = [9]
+toRemove = [9,8,15,21]
 corridor = corridor[0], [cell for k,cell in enumerate(corridor[1]) if not (k in toRemove)]
-#VIEW(hpc_corridor)
-
 
 #Door1
-toMerge = 7
-doorDiagram = assemblyDiagramInit([3,1,2])([[8,1,2.7],[.3],[2.4,.3]])
+toMerge = 19
+doorDiagram = assemblyDiagramInit([3,1,2])([[1.3,1,6.5],[.3],[2.4,.3]])
 corridor = diagram2cell(doorDiagram,corridor,toMerge)
 hpc_corridor = SKEL_1(STRUCT(MKPOLS(corridor)))
 hpc_corridor = cellNumbering (corridor,hpc_corridor)(range(len(corridor[1])),CYAN,2)
-#VIEW(hpc_corridor)
 
-toRemove = [18]
+
+toRemove = [27]
 corridor = corridor[0], [cell for k,cell in enumerate(corridor[1]) if not (k in toRemove)]
 
 #Door2
-toMerge = 9
-doorDiagram = assemblyDiagramInit([3,1,2])([[1.6,1,4.7],[.3],[2.4,.3]])
+toMerge = 16
+doorDiagram = assemblyDiagramInit([3,1,2])([[1.4,1,6.5],[.3],[2.4,.3]])
 corridor = diagram2cell(doorDiagram,corridor,toMerge)
 hpc_corridor = SKEL_1(STRUCT(MKPOLS(corridor)))
 hpc_corridor = cellNumbering (corridor,hpc_corridor)(range(len(corridor[1])),CYAN,2)
-#VIEW(hpc_corridor)
 
-toRemove = [22]
+toRemove = [31]
 corridor = corridor[0], [cell for k,cell in enumerate(corridor[1]) if not (k in toRemove)]
 
-#VIEW(hpc_corridor)
-
-#Entrance
-toMerge = 12
+#Window
+toMerge = 3
 doorDiagram = assemblyDiagramInit([1,3,2])([[.3],[3,1,3],[2.4,.3]])
 corridor = diagram2cell(doorDiagram,corridor,toMerge)
 hpc_corridor = SKEL_1(STRUCT(MKPOLS(corridor)))
 hpc_corridor = cellNumbering (corridor,hpc_corridor)(range(len(corridor[1])),CYAN,2)
-#VIEW(hpc_corridor)
 
-toRemove = [26]
+#########################
+cor_w = [35]
+cor_w = corridor[0], [cell for k,cell in enumerate(corridor[1]) if (k in cor_w)]
+cor_w = COLOR(WINDOW)((STRUCT(MKPOLS(cor_w))))
+cor_w = MK_TRNSP(cor_w)
+#########################
+
+toRemove = [35]
 corridor = corridor[0], [cell for k,cell in enumerate(corridor[1]) if not (k in toRemove)]
 
-#DRAW(corridor)
-
 """Stairs"""
-stair = spiralStair(width=0.2,R=3,r=0.25,riser=0.1,pitch=4.4,nturns=1.5,steps=24)
-stair = larApply(r(0,0,3*PI/4))(stair)
-stair = larApply(t(0,-3,0))(stair)
-stairColumn = larApply(t(0,-3,0))(larRod(0.25,4.2)())
-stairs3D = evalStruct(Struct([stairColumn,stair,t(0,0,4)]*4))
-stairs = (STRUCT(CAT(AA(MKPOLS)(stairs3D))))
+step1 = CUBOID([1,0.5,0.2])
+
+step2 = T([2,3])([0.4,0.2])(step1)
+step3 = T([2,3])([0.8,0.4])(step1)
+step4 = T([2,3])([1.2,0.6])(step1)
+step5 = T([2,3])([1.6,0.8])(step1)
+step6 = T([2,3])([2.0,1.0])(CUBOID([1,1,0.2]))
+stair1 = STRUCT([step1, step2, step3, step4, step5, step6])
+
+stair_floor = STRUCT([stair1, T([1,2,3])([0,2,1.3])(R([1,2])(PI/2)(stair1))
+					,T([1,2,3])([-2,2,2.6])(R([1,2])(PI)(stair1)),
+   					T([1,2,3])([-2,0,3.5])(R([1,2])(3*PI/2)(stair1))
+   					])
+#stair_floor = R([1,2])(PI/2)(stair_floor)
+#VIEW(stair_floor)
 
 """Hill"""
 c10 = larBezier(S1)([[0,0,0],[10,0,0]])
@@ -75,20 +106,48 @@ dom2D = larModelProduct([dom, dom])
 out = larMap(larCoonsPatch([c10,c11,c20,c21]))(dom2D)
 
 
-"""Assembling"""
 
+"""Struct balcone"""
+balcone = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+balcone = master[0], [cell for k,cell in enumerate(master[1]) if (k in balcone)]
+balcone = (STRUCT(MKPOLS(balcone)))
+
+balcone2 = [219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,
+			238,239,240,241]
+balcone2 = master[0], [cell for k,cell in enumerate(master[1]) if (k in balcone2)]
+balcone2 = (STRUCT(MKPOLS(balcone2)))
+
+
+toRemove = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+#VIEW(balcone)
+
+"""Assembling"""
+column = CUBOID([1,3,4])
+column_couple = STRUCT([T([1,2,3])([6,0,-4])(column), T([1,2,3])([12,0,-4])(column)])
 master = COLOR(pink9)(STRUCT(MKPOLS(master)))
+master = STRUCT([balcone, master, balcone2, hall_door, hall_window, hall_doorw,kitch_door,kitch_doorw,
+				s_door, n_door, w_door, sw_door, e_door, room_win, room_winr, room_win2])
+
 top = STRUCT(MKPOLS(top))
-corridor = (STRUCT(MKPOLS(corridor)))
+corridor = COLOR(pink9)(STRUCT(MKPOLS(corridor)))
+corridor = STRUCT([corridor, T([1,2,3])([4.5,2,-4])(stair_floor)])
+Corridor_base = (STRUCT(MKPOLS(Corridor_base)))
 master_rotate = R([1,2])(160.222)(master)
 hill = R([1,2])(79.999)(COLOR(GREEN)(STRUCT(MKPOLS(out))))
 
-floor = STRUCT([master, T([2])([-18.5])(corridor), (T([1,2])([11.65,-18.5])(master_rotate))])
-roof = STRUCT([top, T([2])([-18.5])(top), (T([2])([-37])(top))])
-dwelling = STRUCT([floor, T(3)(3)(floor), T(3)(6)(floor), T(3)(9)(floor), T(3)(12)(floor), T(3)(15)(roof), T([1,2])([8,-5])(stairs)])
+floor = STRUCT([T(2)(-10.6)(master), T([1,2])([2,-16.2])(corridor), T([1,2])([2,-16.2])(cor_w), (T([1,2])([18.3,-16.2])(master_rotate)), 
+				])
+roof = T([3])([21])(CUBOID([17, 40, 3]))
+dwelling = STRUCT([T([1,2,3])([2,-16,-4])(Corridor_base), column_couple, T(2)(-30)(column_couple),
+				floor, T(3)(3)(floor), T(3)(6)(floor), T(3)(9)(floor), T(3)(12)(floor), #T(3)(15)(roof), T([1,2])([8,-5])(stairs)
+				T(3)(15)(floor),T(3)(18)(floor), T([1,2,3])([1.2,-33.5,0])(roof)])
+
+VIEW(dwelling)
+
 ground = STRUCT([T(2)(-10)(hill), T(2)(17)(hill)])
 
 
 model = STRUCT([T([1,2,3])([5,-3.5,4.5])(dwelling), ground])
 
-VIEW(model)
+#VIEW(model)
